b5cbe118ad08dc37c181a43d1af4027c28d75556
Sienna Bates <the.strawberry.almond.milk@gmail.com>
2023-12-17 13:40:09 -0500

    added multiple tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,21 +1,33 @@
-it('should calculate the monthly rate correctly', function () {
+it('should calculate the monthly rate correctly', () => {
   let values = {
     amount: 75000,
     years: 10,
     rate: 6
   }
   expect(calculateMonthlyPayment(values)).toEqual("832.65");
-  values = {
+});
+
+it('should return a result with 2 decimal places', () => {
+  let values = {
+    amount: 30000,
+    years: 5,
+    rate: 11
+  }
+  expect(calculateMonthlyPayment(values)).toEqual("652.27");
+});
+
+describe("invalid inputs", () => {
+  it('should return NaN if invalid inputs are submitted', () => {
+  let values = {
     amount: 0,
     years: 0,
     rate: 0
   }
   expect(calculateMonthlyPayment(values)).toEqual("NaN");
-});
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
+  values = {
+    amount: "testing",
+    years: "incorrect",
+    rate: false
+  }
+  expect(calculateMonthlyPayment(values)).toEqual("NaN");
+  })})
